a6377040ca0d6b0c8bbe042e0a201b9d350addc4
Shixn <shixn75@gmail.com>
2022-03-25 17:13:13 +0900

    [update] マニュアルの修正.

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,7 +1,17 @@
 (function($) {
 
+  $('a').on('click', function() {
+    const href = $(this).attr('href');
+    if (href.includes('#')) {
+      const target = href.split('#')[1];
+      const offset = $('#' + target).offset().top;
+      $('body, html').animate({scrollTop: offset}, 400, 'easeOutCubic');
+    }
+  });
   
-  /***** easing settings *****/
+  /*
+   * easing settings 
+   */
   jQuery.extend( jQuery.easing, {
     easeOutCubic: function (x, t, b, c, d) {
       return c*((t=t/d-1)*t*t + 1) + b;
